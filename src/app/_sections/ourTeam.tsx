5a06465595d6d5c4f2b1238e6dbeb4c8ef1844d1
BrishmanCL <brishmancl@gmail.com>
2025-08-27 11:14:31 -0500

    update Our Team

--- src/app/_sections/ourTeam.tsx
+++ src/app/_sections/ourTeam.tsx
@@ -19,37 +19,49 @@ export default function OurTeam() {
                     </div>
                     <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 md:mx-auto">
                         <div className="bg-white rounded-lg p-4 shadow-lg">
-                            <Image
-                                className="rounded"
-                                src="https://integertel.com/images/img-single/img-clients.jpg"
-                                width={177}
-                                height={177}
-                                alt=""
-                            />
+                            {
+                                /*
+                                <Image
+                                    className="rounded"
+                                    src="https://integertel.com/images/img-single/img-clients.jpg"
+                                    width={177}
+                                    height={177}
+                                    alt=""
+                                />
+                                */
+                            }
                             <h5 className="font-bold mt-1 text-[#003697]">Brishman Neeson</h5>
                             <span className="text-gray-600">CEO</span>
                             <p className="text-black">Administrador de Sistemas con experiencia en desarrollo web, automatización y soluciones empresariales.</p>
                         </div>
                         <div className="bg-white rounded-lg p-4 shadow-lg">
-                            <Image
-                                className="rounded"
-                                src="https://integertel.com/images/img-single/img-clients.jpg"
-                                width={177}
-                                height={177}
-                                alt=""
-                            />
+                            {
+                                /* 
+                                <Image
+                                    className="rounded"
+                                    src="https://integertel.com/images/img-single/img-clients.jpg"
+                                    width={177}
+                                    height={177}
+                                    alt=""
+                                />
+                                */
+                            }
                             <h5 className="font-bold mt-1 text-[#003697]">Gustavo</h5>
                             <span className="text-gray-600">CTO</span>
                             <p className="text-black">Profesional en Redes y seguridad informática con sólidos conocimientos en el análisis, planeamiento y ejecución de soluciones de Sistemas.</p>
                         </div>
                         <div className="bg-white rounded-lg p-4 shadow-lg">
-                            <Image
-                                className="rounded"
-                                src="https://integertel.com/images/img-single/img-clients.jpg"
-                                width={177}
-                                height={177}
-                                alt=""
-                            />
+                            {
+                                /*
+                                <Image
+                                    className="rounded"
+                                    src="https://integertel.com/images/img-single/img-clients.jpg"
+                                    width={177}
+                                    height={177}
+                                    alt=""
+                                />
+                                */
+                            }
                             <h5 className="font-bold mt-1 text-[#003697]">Roberto</h5>
                             <span className="text-gray-600">Lead Developer</span>
                             <p className="text-black">
