4bbb6056f6093dfdb983b06b726e977a3dd34a86
BrishmanCL <brishmancl@gmail.com>
2025-09-03 15:30:07 -0500

    google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,19 +23,18 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
-        {/* Google Analytics */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-3L55L76L99"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-3L55L76L99');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WKCPGXSB');
           `}
         </Script>
+        {/* End Google Tag Manager */}
+
         <Script
           id="meta-pixel"
           strategy="afterInteractive"
@@ -67,6 +66,17 @@ export default function RootLayout({
       <body
         className={`${ptSans.className} antialiased bg-gradient-to-r from-slate-100 via-slate-300 to-slate-100 text-white`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WKCPGXSB"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <Header></Header>
         {children}
         <Footer></Footer>
