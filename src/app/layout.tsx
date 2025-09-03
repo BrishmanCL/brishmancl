5bad97a0ee4dfcbeb92cda572337f84c261eb6c3
BrishmanCL <brishmancl@gmail.com>
2025-09-03 11:55:54 -0500

    update favicon.ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,17 @@ import "./ui/styles/globals.css";
 import Header from "./ui/components/Header";
 import Footer from "./ui/components/Footer";
 import Script from "next/script";
-import Image from "next/image";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    default: "Brishman CL S.A.C | Empresa TI de soluciones empresariales",
+    template: "%s"
+  },
+  icons: {
+    icon: "/images/Brishman CL logo.svg",
+  }
+};
 
 export default function RootLayout({
   children,
@@ -13,11 +23,11 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
-      <Script
-        id="meta-pixel"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
             !function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
             n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -29,17 +39,17 @@ export default function RootLayout({
             fbq('init', '2643255309304610');
             fbq('track', 'PageView');
           `,
-        }}
-      />
-      <noscript>
-        <Image
-          alt=""
-          height="1"
-          width="1"
-          style={{ display: "none" }}
-          src={`https://www.facebook.com/tr?id=2643255309304610&ev=PageView&noscript=1`}
+          }}
         />
-      </noscript>
+        {/* Meta Pixel (noscript) */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2643255309304610&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
       <body
         className={`${ptSans.className} antialiased bg-gradient-to-r from-slate-100 via-slate-300 to-slate-100 text-white`}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,6 @@ import type { Metadata } from 'next';
 export const metadata: Metadata = {
   title: "Desarrollo Web, Marketing y Automatización | BrishmanCL S.A.C.",
   description: "Creamos sitios web profesionales, campañas de marketing, automatización con WhatsApp y soluciones digitales para empresas. Empieza tu proyecto con BrishmanCL hoy.",
-  icons: {
-    icon: "/images/Brishman CL logo.svg",
-  }
 };
 
 export default function Home() {
